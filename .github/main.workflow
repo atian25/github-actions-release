39e023da5987487dc113a0aca78ce7044dc5035b
TZ | 天猪 <atian25@qq.com>
2019-03-25 23:47:08 +0800

    feat: test workflow (#1)

--- .github/main.workflow
+++ .github/main.workflow
@@ -1,9 +1,16 @@
 workflow "Unit Test" {
-  on = "push"
-  resolves = ["github-actions-release"]
+  resolves = ["github-actions-release", "act2"]
+  on = "pull_request"
 }
 
 action "github-actions-release" {
   uses = "./"
-  args = "check"
+  args = "check --test=abc"
+  secrets = ["GITHUB_TOKEN"]
+}
+
+action "act2" {
+  uses = "./"
+  args = "deploy --a=bb"
+  secrets = ["GITHUB_TOKEN"]
 }
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 'use strict';
 
 const { Toolkit } = require('actions-toolkit');
-const tools = new Toolkit();
+const tools = new Toolkit({
+  // event: [ 'push', 'pull_requests' ],
+});
 
-console.log(tools.arguments);
+async function run() {
+
+  console.log(tools.arguments);
+
+  console.log('@@@event', JSON.stringify(tools.context.event, null, 2));
+
+  console.log('@@@payload', JSON.stringify(tools.context.payload, null, 2));
+
+  const pkg = tools.getPackageJSON();
+
+  console.log('@@@pkg', pkg);
+
+  const contents = tools.getFile('index.js');
+
+  console.log('@@@contents', contents);
+
+  const files = await tools.github.pulls.listFiles(tools.context.repo({
+    number: tools.context.payload.number,
+  }));
+
+  console.log('@@@files', files);
+}
+
+run();
