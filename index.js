76cd594e3ab5edc2ff73c0a46d7609ff85f66678
TZ <atian25@qq.com>
2019-03-25 16:12:03 +0800

    feat: test workflow

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const { Toolkit } = require('actions-toolkit');
 const tools = new Toolkit();
 
 console.log(tools.arguments);
+
+tools.command('check', async (args, match) => {
+  console.log(args)
+  // -> { app: 'example', container: 'node:alpine' }
+});
+
